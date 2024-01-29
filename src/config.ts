442618ddddc55aca6036ad3004c640e844fa9214
rehtropsmb <kingretrop@gmail.com>
2024-01-29 01:57:43 -0600

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -3,87 +3,75 @@ const worldStart = 1;
 const worldCount = 1;
 
 // REQUIRED BANANAS
-const bananas = 35;
+const bananas = 550;
 
-// FULL STRAT LIST
-const asgStrats = [
-    {
-        name: "asg",
-        stage: "1-1",
-        time: 92,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-2",
-        time: 90,
-        bananas: 24,
-    },
-    {
-        name: "asg",
-        stage: "1-3",
-        time: 90,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-4",
-        time: 182,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-5",
-        time: 82,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-6",
-        time: 287,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-7",
-        time: 274,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-8",
-        time: 288,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-9",
-        time: 172,
-        bananas: 0,
-    },
-    {
-        name: "asg",
-        stage: "1-10",
-        time: 287,
-        bananas: 0,
-    },
+const s1_1 = [
+    { stage: '1-1', time: 92, bananas: 0, name: 'asg (fast)' },
+    { stage: '1-1', time: 90, bananas: 0, name: 'asg (safe)' },
+    { stage: '1-1', time: 84, bananas: 52, name: 'sweep (safe)' },
+    { stage: '1-1', time: 86.5, bananas: 50, name: 'sweep (scary)' },
 ];
 
-const bunchStrats = [
-    {
-        name: "one bunch",
-        stage: "1-1",
-        time: 90,
-        bananas: 24,
-    },
-    {
-        name: "singles",
-        stage: "1-3",
-        time: 80,
-        bananas: 9,
-    },
+const s1_2 = [
+    { stage: '1-2', time: 91, bananas: 0, name: 'asg' },
+    { stage: '1-2', time: 86, bananas: 40, name: 'sweep' },
 ];
 
-const strats = asgStrats.concat(bunchStrats);
+const s1_3 = [
+    { stage: '1-3', time: 90, bananas: 0, name: 'asg' },
+    { stage: '1-3', time: 86, bananas: 40, name: 'score' },
+];
+
+const s1_4 = [
+    { stage: '1-4', time: 192, bananas: 10, name: 'asg' },
+    { stage: '1-4', time: 186, bananas: 30, name: '2 bunch' },
+    { stage: '1-4', time: 170, bananas: 80, name: '7 bunch' },
+    { stage: '1-4', time: 149, bananas: 140, name: '13 bunch' },
+];
+
+const s1_5 = [
+    { stage: '1-5', time: 90, bananas: 0, name: 'asg' },
+    { stage: '1-5', time: 80, bananas: 30, name: 'slow 3 bunch' },
+];
+
+const s1_6 = [
+    { stage: '1-6', time: 287, bananas: 0, name: 'asg' },
+    { stage: '1-6', time: 276, bananas: 30, name: '3 bunch' },
+];
+
+const s1_7 = [
+    { stage: '1-7', time: 285, bananas: 0, name: 'asg' },
+    { stage: '1-7', time: 150, bananas: 160, name: 'slow sweep' },
+];
+
+const s1_8 = [
+    { stage: '1-8', time: 288, bananas: 0, name: 'asg' },
+    { stage: '1-8', time: 260, bananas: 110, name: 'score' },
+];
+
+const s1_9 = [
+    { stage: '1-9', time: 175, bananas: 0, name: 'asg' },
+    { stage: '1-9', time: 170, bananas: 50, name: 'bunch' },
+];
+
+const s1_10 = [
+    { stage: '1-10', time: 282, bananas: 0, name: 'asg' },
+    { stage: '1-10', time: 160, bananas: 270, name: 'slow sweep' },
+];
+
+const w1 = [
+    ...s1_1,
+    ...s1_2,
+    ...s1_3,
+    ...s1_4,
+    ...s1_5,
+    ...s1_6,
+    ...s1_7,
+    ...s1_8,
+    ...s1_9,
+    ...s1_10,
+];
+
+const strats = [...w1];
 
 export { strats, bananas, worldStart, worldCount };
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,19 @@ const constraints: any = {};
 const variables: any = {};
 const ints: any = {};
 
-for (let k = (worldStart-1)*10; k < (worldStart-1)*10 + (worldCount*10); k++) {
-    constraints[`${Math.floor((k/10)+1)}-${(k%10)+1}`] = { equal: 1};
+for (
+    let k = (worldStart - 1) * 10;
+    k < (worldStart - 1) * 10 + worldCount * 10;
+    k++
+) {
+    constraints[`${Math.floor(k / 10 + 1)}-${(k % 10) + 1}`] = { equal: 1 };
 }
 constraints.bananas = { min: bananas };
 
 for (const strat of strats) {
-    const ids = strat.stage.split('-').map(val => parseInt(val));
+    const ids = strat.stage.split('-').map((val: string) => parseInt(val));
     variables[`${strat.stage}-${strat.name}`] = {
-        time: timers[(ids[0]-1) * 10 + (ids[1]-1)] - strat.time,
+        time: timers[(ids[0] - 1) * 10 + (ids[1] - 1)] - strat.time,
         bananas: strat.bananas,
     };
     variables[`${strat.stage}-${strat.name}`][`${strat.stage}`] = 1;
@@ -33,8 +37,7 @@ const results = solver.Solve(model);
 
 if (!results.feasible) {
     console.log('The required banana count cannot be reached!');
-}
-else {
+} else {
     const list: string[] = [];
     for (const key in results) {
         if (key.includes('-')) {
@@ -42,10 +45,13 @@ else {
         }
     }
     list.sort((a: string, b: string): number => {
-        const id_a = a.split('-').map(val => parseInt(val));
-        const id_b = b.split('-').map(val => parseInt(val));
-        if (id_a[0] > id_b[0]) { return id_a[0] - id_b[0]; }
-        else { return id_a[1] - id_b[1]; }
+        const id_a = a.split('-').map((val) => parseInt(val));
+        const id_b = b.split('-').map((val) => parseInt(val));
+        if (id_a[0] > id_b[0]) {
+            return id_a[0] - id_b[0];
+        } else {
+            return id_a[1] - id_b[1];
+        }
     });
     console.log(list);
 }
