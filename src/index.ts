a2e39222aec080fe760b67620160af58461cbf24
rehtropsmb <kingretrop@gmail.com>
2024-09-13 12:40:20 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { strats, bananas, worldStart, worldCount } from './config';
 import timers from './timers';
+import * as fs from 'fs';
+// import { strats } from './config';
+
+// CONFIG VARAIABLES
+const bananas = 5_000;
+
+// IMPORT CSV
+const stratFile = 'data/10k asg - strat data.csv';
+const csvToJson = require('convert-csv-to-json');
+const strats = csvToJson.fieldDelimiter(',').getJsonFromCsv(stratFile);
+
+for (const strat of strats) {
+    strat.igt = parseFloat(strat.igt);
+    strat.bananas = parseInt(strat.bananas);
+}
 
 // CALCULATION CODE
 const solver = require('javascript-lp-solver');
@@ -7,25 +21,32 @@ const constraints: any = {};
 const variables: any = {};
 const ints: any = {};
 
+// create the following constraint for every stage:
+// constraints["1-1"] = { equal: 1};
+// this will force exactly one strat to be picked for each stage
 for (
-    let k = (worldStart - 1) * 10;
-    k < (worldStart - 1) * 10 + worldCount * 10;
-    k++
-) {
+    let k = 0; k < 100; k++) {
     constraints[`${Math.floor(k / 10 + 1)}-${(k % 10) + 1}`] = { equal: 1 };
 }
+
+// this sets the minimum required banana count
 constraints.bananas = { min: bananas };
 
+// create variables for each strat
+// variables["1-1_example-strat"] = { time: 85, bananas: 40, "1-1": 1 };
+// ints["1-1_example-strat"] = 1;
+// these create the options that will be picked from
 for (const strat of strats) {
     const ids = strat.stage.split('-').map((val: string) => parseInt(val));
-    variables[`${strat.stage}-${strat.name}`] = {
-        time: timers[(ids[0] - 1) * 10 + (ids[1] - 1)] - strat.time,
+    variables[`${strat.stage}_${strat.name}`] = {
+        time: timers[(ids[0] - 1) * 10 + (ids[1] - 1)] - strat.igt,
         bananas: strat.bananas,
     };
-    variables[`${strat.stage}-${strat.name}`][`${strat.stage}`] = 1;
-    ints[`${strat.stage}-${strat.name}`] = 1;
+    variables[`${strat.stage}_${strat.name}`][`${strat.stage}`] = 1;
+    ints[`${strat.stage}_${strat.name}`] = 1;
 }
 
+//  SOLVE MODEL
 const model = {
     optimize: 'time',
     opType: 'min',
@@ -35,23 +56,27 @@ const model = {
 };
 const results = solver.Solve(model);
 
+// FORMAT RESULTS
 if (!results.feasible) {
     console.log('The required banana count cannot be reached!');
 } else {
-    const list: string[] = [];
+    let list: string[] = [];
     for (const key in results) {
         if (key.includes('-')) {
             list.push(key);
         }
     }
     list.sort((a: string, b: string): number => {
-        const id_a = a.split('-').map((val) => parseInt(val));
-        const id_b = b.split('-').map((val) => parseInt(val));
-        if (id_a[0] > id_b[0]) {
-            return id_a[0] - id_b[0];
-        } else {
-            return id_a[1] - id_b[1];
-        }
+        const id_a = a.split('_')[0].split('-').map(value => parseInt(value));
+        const id_b = b.split('_')[0].split('-').map(value => parseInt(value));
+        const int_a = id_a[0] * 10 + id_a[1];
+        const int_b = id_b[0] * 10 + id_b[1];
+        return int_a - int_b;
     });
-    console.log(list);
+    let output = '';
+    for (let text of list) {
+        output += `${text}\n`;
+    }
+    fs.writeFileSync('data/output.txt', output, {});
+    console.log('Result output to data/output.txt');
 }
